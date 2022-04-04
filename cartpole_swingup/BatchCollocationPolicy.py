8a2e117cbf9b10f5160fd89d0bc2eebf120ee917
Shivesh Khaitan <shiveshkhaitan@gmail.com>
2022-04-04 18:12:07 -0400

    Collocation working

--- cartpole_swingup/BatchCollocationPolicy.py
+++ cartpole_swingup/BatchCollocationPolicy.py
@@ -14,66 +14,70 @@ class BatchCollocationPolicy:
         self.iters = iters
         self.lr = lr
         self.N = N
+        self.rho = 200
         self.mpc_env = gym.make("ModifiedTorchCartPoleSwingUp-v0")
-        self.mpc_env.mpc_reset(self.N)
-        self.states = torch.tensor(data=torch.zeros(self.N, 4, self.T).float(),
+        self.mpc_env.mpc_reset(self.N * self.T)
+        self.states = torch.tensor(data=torch.zeros(self.N * (self.T + 1), 4).float(),
                                    device=torch.device('cuda:0'),
                                    requires_grad=True)
-        self.actions = torch.tensor(data=torch.FloatTensor(self.N, self.T).uniform_(-1.0, 1.0),
+        self.actions = torch.tensor(data=torch.FloatTensor(self.N * self.T).uniform_(-1.0, 1.0),
                                     device=torch.device('cuda:0'),
                                     requires_grad=True)
-        self.lambdas = torch.tensor(data=torch.ones(self.N, 4, self.T).float(),
-                                    device=torch.device('cuda:0'),
-                                    requires_grad=True)
+        self.lambdas = torch.tensor(data=torch.ones(self.N * self.T).float(),
+                                    device=torch.device('cuda:0'))
 
-    def __call__(self, state, warm_start=False):
+        indices = torch.arange(self.N * (self.T + 1))
+        self.initial_indices = indices.reshape(self.N, self.T + 1)[:, :-1].reshape(self.N * self.T)
+        self.final_indices = indices.reshape(self.N, self.T + 1)[:, 1:].reshape(self.N * self.T)
+
+    def __call__(self, state, warm_start=False, skip=1):
         if warm_start:
-            self.actions.data[:, :-1] = self.actions.clone().data[:, 1:]
+            self.actions.data[:-skip] = self.actions.clone().data[skip:]
+
             self.mpc_env.mpc_reset(state=state)
+            self.states.data[::self.T + 1] = self.mpc_env.state
             for t in range(self.T):
-                self.mpc_env.step(self.actions[:, t].reshape(self.N, 1))
-                self.states.data[:, :, t] = self.mpc_env.state
-            self.lambdas.data[:, :, :-1] = self.lambdas.clone().data[:, :, 1:]
+                self.mpc_env.step(self.actions[t::self.T].reshape(self.N, 1))
+                self.states.data[t + 1::self.T + 1] = self.mpc_env.state
+
+            self.lambdas = torch.tensor(data=torch.ones(self.N * self.T).float() * 500,
+                                        device=torch.device('cuda:0'))
         else:
             self.states.data *= 0.0
-            self.actions = torch.tensor(data=torch.FloatTensor(self.N, self.T).uniform_(-1.0, 1.0),
-                                        device=torch.device('cuda:0'), requires_grad=True)
-            self.lambdas *= 0.0
-
-        optimizer = torch.optim.SGD({self.states, self.actions}, lr=0.01, momentum=0.9)
-        lambda_optimizer = torch.optim.SGD({self.lambdas}, lr=0.1, momentum=0.9)
+            self.states.data[::self.T + 1] = state
+            self.actions.data = torch.tensor(data=torch.FloatTensor(self.N * self.T).uniform_(-1.0, 1.0),
+                                             device=torch.device('cuda:0'), requires_grad=True)
+            self.lambdas = torch.tensor(data=torch.ones(self.N * self.T).float(),
+                                        device=torch.device('cuda:0'))
 
         for i in range(self.iters):
-            for j in range(1):
-                optimizer.zero_grad()
-                self.mpc_env.mpc_reset(state=state)
-                rewards = torch.tensor(0.0, device=torch.device('cuda:0')).repeat(self.N, 4)
-                for t in range(self.T):
-                    self.mpc_env.step(self.actions[:, t].reshape(self.N, 1))
-                    rewards -= self.states[:, 2, t].cos() - abs(self.states[:, 0, t])
-                    rewards += self.lambdas[:, :, t] * ((self.states[:, :, t] - self.mpc_env.state) ** 2)
-                    self.mpc_env.mpc_reset(state=self.states[:, :, t])
+            state_optimizer = torch.optim.Adam({self.states}, lr=0.1)
+            action_optimizer = torch.optim.Adam({self.actions}, lr=0.05)
+
+            for j in range(500):
+                state_optimizer.zero_grad()
+                action_optimizer.zero_grad()
+                self.states.data[::self.T + 1] = state
+                self.mpc_env.mpc_reset(state=self.states[self.initial_indices])
+                self.mpc_env.step(self.actions.reshape(self.N * self.T, 1))
+                rewards = torch.tensor(0.0, device=torch.device('cuda:0')).repeat(self.N * self.T)
+                rewards -= self.states[self.final_indices, 2].cos() - abs(self.states[self.final_indices, 0])
+                rewards += self.lambdas * (torch.sum((self.states[self.final_indices] - self.mpc_env.state) ** 2, dim=1) - 1e-5)
+                rewards += 0.5 * self.rho * (torch.sum((self.states[self.final_indices] - self.mpc_env.state) ** 2, dim=1) ** 2)
+
                 rewards = torch.sum(rewards)
                 rewards.backward()
-                optimizer.step()
+                state_optimizer.step()
+                action_optimizer.step()
 
                 with torch.no_grad():
                     self.actions.data = torch.clamp(self.actions, -1.0, 1.0)
 
-            lambda_optimizer.zero_grad()
-            self.mpc_env.mpc_reset(state=state)
-            rewards = torch.tensor(0.0, device=torch.device('cuda:0')).repeat(self.N, 4)
-            for t in range(self.T):
-                self.mpc_env.step(self.actions[:, t].reshape(self.N, 1))
-                rewards -= self.lambdas[:, :, t] * ((self.states[:, :, t] - self.mpc_env.state) ** 2)
-                rewards += self.states[:, 2, t].cos() - abs(self.states[:, 0, t])
-                self.mpc_env.mpc_reset(state=self.states[:, :, t])
-            rewards = torch.sum(rewards)
-            rewards.backward()
-            lambda_optimizer.step()
-
-            with torch.no_grad():
-                self.lambdas.data = torch.clamp(self.lambdas, 0.0, 999999999999999)
+            self.states.data[::self.T + 1] = state
+            self.mpc_env.mpc_reset(state=self.states[self.initial_indices])
+            self.mpc_env.step(self.actions.reshape(self.N * self.T, 1))
+            self.lambdas.data += 0.1 * torch.log(torch.sum((self.states[self.final_indices] - self.mpc_env.state) ** 2, dim=1) / 1e-5 + 0.01) * self.lambdas
+            self.lambdas.data = torch.clamp(self.lambdas, 0.0, 500000000000.0)
 
         return self.states, self.actions
 
@@ -81,14 +85,15 @@ class BatchCollocationPolicy:
 if __name__ == "__main__":
     env = gym.make('ModifiedTorchCartPoleSwingUp-v0')
     N = 1
+    T = 70
     env.mpc_reset(N)
 
     done = False
 
-    policy = BatchCollocationPolicy(N=N)
+    policy = BatchCollocationPolicy(T=T, iters=30, N=N)
 
     for i in range(500):
-        _, actions = policy(env.state.detach(), warm_start=True)
-        env.step(actions)
-        if i % 5 == 0:
+        states, actions = policy(env.state.detach(), warm_start=True, skip=10)
+        for j in range(10):
+            env.step(actions[j::T].reshape(N, 1))
             env.render()
