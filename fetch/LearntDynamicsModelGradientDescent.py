8982e03085f588f7c00d4c1db35a5058ee49b799
Shivesh Khaitan <shiveshkhaitan@gmail.com>
2022-04-08 16:15:48 -0400

    Collect and train dynamics model with GD

--- fetch/LearntDynamicsModelGradientDescent.py
+++ fetch/LearntDynamicsModelGradientDescent.py
@@ -7,9 +7,8 @@ import torch
 import sys
 sys.path.append('../')
 
-from learn_dynamics import MLP_Dynamics
+from learn_dynamics import MLP_Dynamics, LearnDynamics
 
-from configs import observation_dim, action_dim, hidden_size, num_hidden
 torch.set_printoptions(sci_mode=False)
 
 
@@ -55,14 +54,17 @@ class LearntModelGradientDescentPolicy:
             rewards = 0
             for t in range(self.T):
                 current_state = current_state + (self.dynamics_model(((torch.hstack([current_state, self.actions[:, t].reshape(self.action_dim)]).float() - self.x_mean) / self.x_std).float()) * self.y_std) + self.y_mean
+                current_state = torch.clip(current_state, -10.0, 10.0)
                 if current_state.shape[0] == 10:
                     reward = - ((current_state[0] - goal[0]) ** 2 + (current_state[1] - goal[1]) ** 2 + (current_state[2] - goal[2]) ** 2)
                 else:
                     reward = - ((current_state[3] - goal[0]) ** 2 + (current_state[4] - goal[1]) ** 2 + (current_state[5] - goal[2]) ** 2)
+                    # reward = - ((current_state[3] - current_state[0]) ** 2 + (current_state[4] - current_state[1]) ** 2 + (current_state[5] - current_state[2]) ** 2)
+                    # reward = - ((current_state[0] - goal[0]) ** 2 + (current_state[1] - goal[1]) ** 2 + (current_state[2] - goal[2]) ** 2)
                 rewards -= reward
             rewards.backward()
             optimizer.step()
-
+            self.actions.data = torch.clip(self.actions, -1.0, 1.0)
         return self.actions[:, 0].detach().cpu().numpy()
 
 
@@ -71,31 +73,64 @@ if __name__ == "__main__":
     parser.add_argument('--env-name', type=str, default='FetchReach-v1', help='the environment name')
     parser.add_argument('--filename', type=str, default='dynamics_data/FetchReach_data.npy', help='File name having data')
     parser.add_argument('--model-path', type=str, default='dynamics_models/FetchReach/dm_1.zip', help='Model path')
+    parser.add_argument('--epochs', type=int, default=30000, help='Number of epochs for training')
+    parser.add_argument('--decay-after', type=int, default=10000, help='Decay learning rate after')
+    parser.add_argument('--decay-factor', type=float, default=10.0, help='Learning rate decay factor')
+    parser.add_argument('--learning-rate', type=float, default=0.01, help='Learning rate')
+    parser.add_argument('--num-steps', type=int, default=0, help='Number of steps after which to learn dynamics')
+
     args = parser.parse_args()
 
     env = gym.make(args.env_name)
     obs = env.reset()
 
     data = np.load(args.filename)
-    train_x = data[:, :observation_dim(args.env_name) + action_dim(args.env_name)]
-    train_y = data[:, observation_dim(args.env_name) + action_dim(args.env_name):]
+    train_x = data[:, :obs['observation'].shape[0] + env.action_space.shape[0]]
+    train_y = data[:, obs['observation'].shape[0] + env.action_space.shape[0]:]
 
     x_mean = torch.tensor(train_x.mean(axis=0), device=torch.device('cuda:0'))
     x_std = torch.tensor(train_x.std(axis=0), device=torch.device('cuda:0'))
-    ltrain_y = train_y - train_x[:, :observation_dim(args.env_name)]
+    ltrain_y = train_y - train_x[:, :obs['observation'].shape[0]]
     y_mean = torch.tensor(ltrain_y.mean(axis=0), device=torch.device('cuda:0'))
     y_std = torch.tensor(ltrain_y.std(axis=0), device=torch.device('cuda:0'))
 
-    model = MLP_Dynamics(observation_dim(args.env_name),
-                         action_dim(args.env_name),
-                         hidden_size(args.env_name),
-                         num_hidden(args.env_name)).cuda()
-    model.load_state_dict(torch.load(args.model_path).state_dict())
+    model = torch.load(args.model_path)
 
     done = False
 
-    policy = LearntModelGradientDescentPolicy(model, x_mean, x_std, y_mean, y_std, iters=20, action_dim=action_dim(args.env_name))
+    if args.num_steps:
+        learn_dynamics = LearnDynamics(observation_dim=obs['observation'].shape[0],
+                                       action_dim=env.action_space.shape[0],
+                                       filename=args.filename,
+                                       learning_rate=args.learning_rate,
+                                       epochs=args.epochs,
+                                       decay_factor=args.decay_factor,
+                                       decay_after=args.decay_after,
+                                       model_path=args.model_path)
+        data = np.zeros((args.num_steps, env.action_space.shape[0] + 2 * obs['observation'].shape[0]))
+        index = 0
 
-    while not done:
-        obs, reward, done, _ = env.step(policy(obs, warm_start=True))
-        env.render()
+    policy = LearntModelGradientDescentPolicy(model, x_mean, x_std, y_mean, y_std, iters=20, action_dim=env.action_space.shape[0])
+
+    for _ in range(50000):
+        obs = env.reset()
+        done = False
+        while not done:
+            action = policy(obs, warm_start=True)
+
+            if args.num_steps:
+                data[index, :obs['observation'].shape[0]] = obs['observation']
+                data[index, obs['observation'].shape[0]:obs['observation'].shape[0] + env.action_space.shape[0]] = action
+
+            obs, reward, done, _ = env.step(action)
+
+            if args.num_steps:
+                data[index, obs['observation'].shape[0] + env.action_space.shape[0]:] = obs['observation']
+                index = (index + 1) % args.num_steps
+                if index == 0:
+                    learn_dynamics.append_data(data)
+                    np.save(args.filename, learn_dynamics.data)
+                    policy.model = learn_dynamics.learn_dynamics(new_model=True)
+                    index = 0
+
+            env.render()
--- fetch/learn_dynamics.py
+++ fetch/learn_dynamics.py
@@ -31,6 +31,78 @@ class MLP_Dynamics(torch.nn.Module):
         return x
 
 
+class LearnDynamics:
+
+    def __init__(self, observation_dim, action_dim, filename, learning_rate, epochs, decay_factor, decay_after, hidden_size=200, num_hidden=1, model_path=None, model=None):
+        self.observation_dim = observation_dim
+        self.action_dim = action_dim
+        self.filename = filename
+        self.learning_rate = learning_rate
+        self.epochs = epochs
+        self.decay_factor = decay_factor
+        self.decay_after = decay_after
+        self.hidden_size = hidden_size
+        self.num_hidden = num_hidden
+        self.model_path = model_path
+        self.model = model
+        if self.model is None:
+            self.model = MLP_Dynamics(observation_dim=observation_dim,
+                                      action_dim=action_dim,
+                                      hidden_size=hidden_size,
+                                      num_hidden=num_hidden).cuda()
+        self.data = np.load(self.filename)
+
+    def append_data(self, data):
+        self.data = np.vstack((self.data, data))
+
+    def learn_dynamics(self, new_model=False):
+        if new_model:
+            self.model = MLP_Dynamics(observation_dim=self.observation_dim,
+                                      action_dim=self.action_dim,
+                                      hidden_size=self.hidden_size,
+                                      num_hidden=self.num_hidden).cuda()
+
+        X = torch.as_tensor(self.data[:, :self.observation_dim + self.action_dim]).float().cuda()
+        Y = torch.as_tensor(self.data[:, self.observation_dim + self.action_dim:]).float().cuda()
+        Y = Y - X[:, :self.observation_dim]
+
+        training = random.sample(range(len(X)), int(0.8 * len(X)))
+        validation = list(set(range(len(X))) - set(training))
+
+        train_x = (X[training] - X[training].mean(axis=0)) / X[training].std(axis=0)
+        train_y = (Y[training] - Y[training].mean(axis=0)) / Y[training].std(axis=0)
+
+        validation_x = (X[validation] - X[training].mean(axis=0)) / X[training].std(axis=0)
+        validation_y = (Y[validation] - Y[training].mean(axis=0)) / Y[training].std(axis=0)
+
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=self.learning_rate)
+        loss_fn = torch.nn.MSELoss()
+
+        del X, Y
+
+        for epoch in range(self.epochs):
+            if epoch % self.decay_after == 0:
+                # print('------------------------------------')
+                optimizer.param_groups[0]['lr'] /= self.decay_factor
+
+            optimizer.zero_grad()
+
+            y_pred = self.model(train_x)
+            loss = loss_fn(y_pred, train_y)
+            loss.backward()
+            optimizer.step()
+            # if epoch % int(self.epochs / 20) == 0:
+            #     print(epoch)
+            #     print('Training loss: ', loss)
+            #     y_pred = self.model(validation_x)
+            #     loss = loss_fn(y_pred, validation_y)
+            #     print('Validation loss: ', loss)
+
+        if self.model_path is not None:
+            torch.save(self.model, self.model_path)
+        return self.model
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--env-name', type=str, default='ModifiedTorchCartPoleSwingUp-v0', help='the environment name')
@@ -44,53 +116,17 @@ def main():
     parser.add_argument('--learning-rate', type=float, default=0.01, help='Learning rate')
     args = parser.parse_args()
 
-    model = MLP_Dynamics(observation_dim=observation_dim(args.env_name),
-                         action_dim=action_dim(args.env_name),
-                         hidden_size=args.hidden_size,
-                         num_hidden=args.num_hidden).cuda()
-
-    data = np.load(args.filename)
-    print(data.shape)
-    data = data.reshape(len(data), len(data[0]))
-
-    X = torch.as_tensor(data[:, :observation_dim(args.env_name) + action_dim(args.env_name)]).float().cuda()
-    Y = torch.as_tensor(data[:, observation_dim(args.env_name) + action_dim(args.env_name):]).float().cuda()
-    Y = Y - X[:, :observation_dim(args.env_name)]
-
-    training = random.sample(range(len(X)), int(0.8 * len(X)))
-    validation = list(set(range(len(X))) - set(training))
-
-    train_x = (X[training] - X[training].mean(axis=0)) / X[training].std(axis=0)
-    train_y = (Y[training] - Y[training].mean(axis=0)) / Y[training].std(axis=0)
-    print(train_x.shape, Y[training].std(axis=0))
-
-    validation_x = (X[validation] - X[training].mean(axis=0)) / X[training].std(axis=0)
-    validation_y = (Y[validation] - Y[training].mean(axis=0)) / Y[training].std(axis=0)
-
-    optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate)
-    loss_fn = torch.nn.MSELoss()
-
-    del X, Y
-
-    for epoch in range(args.epochs):
-        if epoch % args.decay_after == 0:
-            print('------------------------------------')
-            optimizer.param_groups[0]['lr'] /= args.decay_factor
-
-        optimizer.zero_grad()
-
-        y_pred = model(train_x)
-        loss = loss_fn(y_pred, train_y)
-        loss.backward()
-        optimizer.step()
-        print(epoch)
-        print('Training loss: ', loss)
-        # if epoch % int(args.epochs / 20) == 0:
-        y_pred = model(validation_x)
-        loss = loss_fn(y_pred, validation_y)
-        print('Validation loss: ', loss)
-
-    torch.save(model, args.model_path)
+    learn_dyanamics = LearnDynamics(observation_dim=observation_dim(args.env_name),
+                                    action_dim=action_dim(args.env_name),
+                                    hidden_size=args.hidden_size,
+                                    num_hidden=args.num_hidden,
+                                    filename=args.filename,
+                                    learning_rate=args.learning_rate,
+                                    epochs=args.epochs,
+                                    decay_factor=args.decay_factor,
+                                    decay_after=args.decay_after,
+                                    model_path=args.model_path)
+    learn_dyanamics.learn_dynamics()
 
 
 if __name__ == '__main__':
